9ce7dca1abe180cc7ad037a33b379fe326bfe95e
Anastasia Pienkina <128829974+anasta9@users.noreply.github.com>
2023-05-10 14:43:35 +0200

    maj class java HairdresserDto

--- src/main/java/entities/HairdresserDto.java
+++ src/main/java/entities/HairdresserDto.java
@@ -1,5 +1,56 @@
 package BeautyConnect;
 
+import java.util.List;
+
 public class HairdresserDto {
+	private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private List<String> services;
+    
+    
+    
+	public HairdresserDto(int id, String name, String email, String phone, List<String> services) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.services = services;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public List<String> getServices() {
+		return services;
+	}
+	public void setServices(List<String> services) {
+		this.services = services;
+	}
+    
+    
 
 }
